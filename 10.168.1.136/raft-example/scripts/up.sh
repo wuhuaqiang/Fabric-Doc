68b1754576bb7929f10d2dd559d97049fbdf426d
whq7402979 <609676374@qq.com>
2019-11-01 14:04:54 +0800

    init

--- 10.168.1.136/raft-example/scripts/up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker-compose -f ../docker-compose-orderer.yaml up -d
-sleep 10
-docker-compose -f ../docker-compose-peer.yaml up -d
-docker-compose -f ../docker-compose-couchdb0.yaml up -d
-docker-compose -f ../docker-compose-cli.yaml up -d
-docker-compose -f ../docker-compose-ca.yaml up -d
--- 10.168.1.138/raft-example/scripts/up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose -f ../docker-compose-orderer.yaml up -d
-sleep 10
-docker-compose -f ../docker-compose-peer.yaml up -d
-docker-compose -f ../docker-compose-cli.yaml up -d
